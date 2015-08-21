50a1e2d2ebd4a7833d302385c06f75489c9298b2
Infatum <qwertty459gmail.com>
2015-08-21 12:01:14 +0300

    First

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
